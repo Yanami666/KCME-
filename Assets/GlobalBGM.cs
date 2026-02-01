ac1925327a1005ad5ae9355445ae2cf332586296
MeganH17 <03alyshamegan@gmail.com>
2026-02-01 02:55:43 -0500

    global bgm

---
+++ Assets/GlobalBGM.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GlobalBGM : MonoBehaviour
+{
+    public AudioClip bgmClip;
+    public float volume = 0.5f;
+
+    private static GlobalBGM instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject); // 防止重复创建
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = bgmClip;
+        audioSource.loop = true;
+        audioSource.volume = volume;
+        audioSource.Play();
+    }
+}
