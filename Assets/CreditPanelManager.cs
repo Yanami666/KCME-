a813cc5e0d97547a9fc043e782317cd7dd1354cd
MeganH17 <03alyshamegan@gmail.com>
2026-02-01 02:29:02 -0500

    starting page panel edited
    panel img added
    close btn sprites

--- Assets/CreditPanelManager.cs
+++ Assets/CreditPanelManager.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CreditPanelManager : MonoBehaviour
 {
     public GameObject creditPanel;
+
+    // Hover sprite references
+    public Image closeButtonImage;
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
 
     public void ShowPanel()
     {
@@ -12,5 +19,22 @@ public class CreditPanelManager : MonoBehaviour
     public void HidePanel()
     {
         creditPanel.SetActive(false);
+    }
+
+    // These methods are called via EventTrigger component on the button
+    public void OnHoverEnter()
+    {
+        if (closeButtonImage != null && hoverSprite != null)
+        {
+            closeButtonImage.sprite = hoverSprite;
+        }
+    }
+
+    public void OnHoverExit()
+    {
+        if (closeButtonImage != null && normalSprite != null)
+        {
+            closeButtonImage.sprite = normalSprite;
+        }
     }
 }
